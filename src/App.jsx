bea79012f04f747751642ed3aaef0c34453a8b60
borgesmj <borgesmj19@gmail.com>
2024-01-11 10:21:42 -0500

    installed: react router dom, settig the first route

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useEffect, useState } from 'react'
-import Header from './Components/Header'
-import CryptoTable from './Components/CryptoTable'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import Header from "./Components/Header";
+import CryptoTable from "./Components/CryptoTable";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-  const [coinData, setCoinData] = useState([])
+  const [coinData, setCoinData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10')
-        setCoinData(response.data)
+        const response = await axios.get(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10"
+        );
+        setCoinData(response.data);
+      } catch (err) {
+        console.log(err);
       }
-      catch (err) {
-        console.log(err)
-      }
-
-    }
-    fetchData()
-  },[])
-
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div className='w-screen h-screen bg-[#1A1A1A] max-w-screen  text-[#FFFFFF] p-4 select-none'>
-      <Header/>
-      <CryptoTable
-          coinData={coinData}
-      />
+    <div className="w-screen h-screen bg-[#1A1A1A] max-w-screen  text-[#FFFFFF] p-4 select-none">
+      <Header />
+      <Routes>
+        <Route
+          path="/crypto-curr/"
+          element={<CryptoTable coinData={coinData} />}
+        ></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
